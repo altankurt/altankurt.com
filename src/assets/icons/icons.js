93f83db778a05582bd5da8da7173fa5eaec664d2
Altan Kurt <aaltankurt@gmail.com>
2023-09-07 04:09:04 +0300

    feat: add icons

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,8 @@
+export { Email } from './email.js';
+export { Github } from './github.js';
+export { Linkedin } from './linkedin.js';
+export { Medium } from './medium.js';
+export { Moon } from './moon.js';
+export { Sunny } from './sun.js';
+export { Twitter } from './twitter.js';
+export { Coffee } from './coffee.js';
