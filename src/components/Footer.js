1838a962bcab30eee5c9d3d6514034025be5f76d
Altan Kurt <aaltankurt@gmail.com>
2023-09-07 04:17:25 +0300

    feat: create Footer.js component

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="container flex justify-center pb-6 pt-6 border-t border-color max-sm:px-4 xl:absolute xl:bottom-0 text-color">
+      <p className="text-xs">
+        You can access the source code of the website on&nbsp;
+        <a
+          href="https://github.com/altankurt/altankurt.dev"
+          rel="noopener noreferrer"
+          target="blank"
+          className="underline font-semibold hover:font-bold"
+        >
+          GitHub.
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
