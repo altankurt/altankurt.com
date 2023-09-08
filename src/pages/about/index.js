7402fba9a30e702daa9227ce2681f8aee77b6727
Altan Kurt <aaltankurt@gmail.com>
2023-09-08 15:07:04 +0300

    feat: create and edit draft about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,55 @@
+export default function About() {
+  return (
+    <section id="about" className="p-6">
+      {
+        <article className="my-8 max-w-7xl ">
+          <section className="">
+            <h1 className="h3 md:h2 pb-4">Hey, I'm Altan ✌️</h1>
+            <p className="paragraph">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <p className="paragraph pt-2">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English. Many desktop
+              publishing packages and web page editors now use Lorem Ipsum as
+              their default model text.
+            </p>
+          </section>
+          <section>
+            <h2 className="h5 md:h4 pt-6 pb-2">PREVIOUSLY</h2>
+            <p className="paragraph">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English. Many desktop
+              publishing packages and web page editors now use Lorem Ipsum as
+              their default model text.
+            </p>
+            <p className="paragraph pt-2">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English. Many desktop
+              publishing packages and web page editors now use Lorem Ipsum as
+              their default model text.
+            </p>
+          </section>
+        </article>
+      }
+    </section>
+  );
+}
