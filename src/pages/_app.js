3a80aa82df82f09fe45effb8a54553bc9ae9c7c9
Altan Kurt <aaltankurt@gmail.com>
2023-09-11 01:34:51 +0300

    feat: Import ThemeProvider, Meta, and Layout to _app.js and include them in the render function

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import Meta from '@/components/meta';
+import { Layout } from '@/components/layout/Layout';
 import '@/styles/globals.css';
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Meta />
+      <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
