16f69f60d2f5322341e2cd1267caf3d692079887
Altan Kurt <aaltankurt@gmail.com>
2023-09-14 14:26:22 +0300

    style: Update h1 styling

--- src/components/blog/posts/post-detail/post-header.js
+++ src/components/blog/posts/post-detail/post-header.js
@@ -6,8 +6,10 @@ const PostHeader = (props) => {
 
   return (
     <header className="relative">
-      <h1 className="h1 mb-2">{title}</h1>
-      <section className="mb-2 flex items-center justify-between  gap-2">
+      <h1 className="text-5xl flex items-center justify-center font-semibold tracking-tight mb-12 leading-normal">
+        {title}
+      </h1>
+      <section className="mb-2 flex items-center justify-between gap-2">
         <DateReadTime date={date} timeToRead={readTime} />
         <ShareSocial slug={slug} title={title} />
       </section>
