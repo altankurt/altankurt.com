0a8f995b4f162fa3e6c5bd1e71219d2911238ffd
Altan Kurt <aaltankurt@gmail.com>
2023-09-14 14:15:03 +0300

    style: Update styling

--- src/components/blog/posts/date-read-time.js
+++ src/components/blog/posts/date-read-time.js
@@ -1,7 +1,7 @@
 import { formattedDate } from '../../../lib/date/formatting-util';
 
 const DateReadTime = ({ date, timeToRead }) => (
-  <div className="flex flex-col items-start font-mono text-xs tracking-tight text-gray-500 dark:text-dark-text/60 sm:flex-row sm:items-center sm:text-sm">
+  <div className="flex flex-col items-start tracking-tight date-color sm:flex-row sm:items-center sm:text-sm">
     <time dateTime={date}>{formattedDate(date)}</time>
     <span className="mx-2 hidden sm:inline-block" aria-hidden="true">
       &middot;
