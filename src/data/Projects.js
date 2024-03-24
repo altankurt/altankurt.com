6abe241923402d0155cf66f7c721e30bb9c2f8db
Altan Kurt <aaltankurt@gmail.com>
2024-03-25 02:53:26 +0300

    Fix import statement and update styling in PostList.js and update project details in Projects.js

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -3,10 +3,43 @@
 export const projects = [
   {
     id: 0,
+    title: 'Time Tracker Web App',
+    description:
+      'The "Time Tracker" project is a web application designed for time management and task tracking. It allows users to track the time spent on various tasks and maintain logs of their activities.',
+    stack: [
+      'React.js',
+      'Firebase',
+      'Firebase SDK',
+      'Zod',
+      'React Router',
+      'TailwindCSS',
+      'Radix UI',
+      'Eslint',
+    ],
+    links: [
+      {
+        link: 'https://github.com/altankurt/time-track-web-app',
+        id: 1,
+      },
+      {
+        link: 'https://time-track-web-app.vercel.app/',
+        id: 2,
+      },
+    ],
+  },
+  {
+    id: 1,
     title: 'altankurt.dev',
     description:
-      'The source code of my personal website, where I showcase my work and writings.',
-    stack: ['React.js', 'Next.js', 'TailwindCSS', 'Preline UI'],
+      'My personal website, crafted with modern technologies, showcases my writings and professional works',
+    stack: [
+      'Next.js',
+      'TailwindCSS',
+      'Preline UI',
+      'Google Analytics',
+      'Markdown',
+      'Eslint',
+    ],
     links: [
       {
         link: 'https://github.com/altankurt/altankurt.dev',
@@ -19,10 +52,19 @@ export const projects = [
     ],
   },
   {
-    id: 1,
+    id: 2,
     title: 'Student Course Hub',
-    description: 'React-based course and student management platform.',
-    stack: ['React.js', 'TailwindCSS', 'Vite.js'],
+    description:
+      'It is a CRM platform developed to enable efficient management of courses and registered students.',
+    stack: [
+      'React.js',
+      'TailwindCSS',
+      'React Router',
+      'React Hooks',
+      'Axios',
+      'Vite.js',
+      'Eslint',
+    ],
     links: [
       {
         link: 'https://github.com/altankurt/student-course-hub',
@@ -35,7 +77,7 @@ export const projects = [
     ],
   },
   {
-    id: 2,
+    id: 3,
     title: 'Twitter UI Clone',
     description:
       'The project faithfully replicates the original Twitter interface with pixel-perfect precision.',
@@ -51,4 +93,4 @@ export const projects = [
       },
     ],
   },
-];
+]
