d5cb08cceeaa61e891aa8cdd780974e43e3158df
Altan Kurt <aaltankurt@gmail.com>
2024-09-02 10:30:26 +0300

    refactor: maintenance

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,6 +4,7 @@ import { Poppins } from 'next/font/google';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
+
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['200', '300', '400', '500', '600', '700', '800'],
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,21 @@
+import React from 'react';
 import Meta from '@/components/Meta';
-import { Layout } from '@/components/Layout/Layout';
 import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
+import Maintenance from '@/pages/maintenance';
+
+const isMaintenanceMode = true; // Bakım modunu etkinleştir
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Meta />
       <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+          {isMaintenanceMode ? (
+            <Maintenance />
+          ) : (
+            null
+          )}
       </ThemeProvider>
     </>
   );
